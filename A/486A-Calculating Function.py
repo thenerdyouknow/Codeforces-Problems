928afd0b8b0a4ef618779efccb2c1d601d71f29c
Venkatesh Thapan <thapanvenkatesh1@gmail.com>
2018-10-24 04:14:42 +0530

    Update 486A-Calculating Function.py

--- A/486A-Calculating Function.py
+++ A/486A-Calculating Function.py
@@ -1,6 +1,6 @@
-//Used Python for this one as its better at handling really big numbers
-//Made no sense to create time making a wrapper to use C++
-// Used formula of (n*n+1) and (n+1/2)^2 to find the odd and even sums to n.
+#Used Python for this one as its better at handling really big numbers
+#Made no sense to create time making a wrapper to use C++
+#Used formula of (n*n+1) and (n+1/2)^2 to find the odd and even sums to n.
 value_of_n = int(input())
 if(int(value_of_n)%2==0):
   even_sum = int(value_of_n/2)*(int(value_of_n/2)+1)
